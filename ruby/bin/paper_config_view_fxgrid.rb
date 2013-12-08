96d4871d92fb5424a9a6042fb83ba2abf2209553
David MacMahon <davidm@astro.berkeley.edu>
2013-12-08 11:29:39 -0800

    Add global input view for fxgrid
    This shows the F engine used for each grid position.

--- ruby/bin/paper_config_view_fxgrid.rb
+++ ruby/bin/paper_config_view_fxgrid.rb
@@ -9,6 +9,10 @@ require 'papercfg'
 # a "high value" baseline.
 
 cell_text = '--'
+row_cells = [cell_text] * 16
+# Build global_grid[0..6][0..15]
+global_grid = []
+7.times {global_grid << row_cells.dup}
 
 fin = ARGV[0] || 'fxin_to_stapol.yml'
 map = PaperCfg.load_file(fin)
@@ -18,7 +22,6 @@ pattern = PaperCfg::PATTERNS['stapol']
 
 for f in 1..8
   # Build grid[0..6][0..15]
-  row_cells = [cell_text] * 16
   grid = []
   7.times {grid << row_cells.dup}
 
@@ -48,12 +51,17 @@ for f in 1..8
       row = row.ord - 'A'.ord
       col = col.to_i
 
-      # Put text in grid
+      # Put text in grid and global_grid
       if grid[row][col] != cell_text
         warn "position for f#{f}#{chip}#{chan} is " +
              "already marked #{grid[row][col]}"
       end
+      if global_grid[row][col] != cell_text
+        warn "global position for f#{f}#{chip}#{chan} is " +
+             "already marked #{global_grid[row][col]}"
+      end
       grid[row][col] = "#{chip}#{chan}"
+      global_grid[row][col] = "f#{f}"
     end
   end
 
@@ -72,9 +80,23 @@ for f in 1..8
 
   puts "  +----------------------------------------------------------------+"
   puts "     0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15"
-
-  if f != 8
-    puts
-    puts
-  end
+  puts
+  puts
 end
+
+# Output global_grid
+puts "PAPER F Engine Global Input Grid (outliers not shown):"
+puts
+puts "     0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15"
+puts "  +----------------------------------------------------------------+"
+
+for row in 0..6
+  print "#{'ABCDEFG'[row]} | "
+  print global_grid[row].join('  ')
+  print " | #{'ABCDEFG'[row]}"
+  puts
+end
+
+puts "  +----------------------------------------------------------------+"
+puts "     0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15"
+
